b1d5bc74f0eb3a6e3360c5dfdabcb5e594c8c672
dmarclyevkl2000 <dmarcly@evk-lease.com>
2020-09-29 14:52:00 -0400

    silly me

--- ___private_secure/super-secret-passwords-file.php
+++ ___private_secure/super-secret-passwords-file.php
@@ -1,7 +1,7 @@
 <?php //include with "./includes/config_setup.php"
 /*****#####     BEGIN DB Credentials     #####*****/
 /* PDO reference variables for /includes/config_setup.php are below 
-    $PDO_host       = '172.20.0.10';
+    $PDO_host       = '127.0.0.1';
     $PDO_port       = '3306';
     $PDO_dbname     = 'jmicrocms';
     $PDO_charset    = 'utf8mb4';
@@ -9,7 +9,7 @@
     $PDO_dbuser       = "jmicrocms";
     $PDO_dbpass       = '$secret_db_password';
 */
-$secret_db_hostname     = '127.0.0.1';
+$secret_db_hostname     = '172.20.0.10';
 $secret_db_portnumber   = '3306';
 $secret_db_name         = 'jmcms_kswt';
 $secret_db_charset      = 'utf8mb4';
